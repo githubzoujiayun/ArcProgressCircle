d37ab633e19eaaacc2bf95a26ea8ba46fa55542e
GinRyan <lp8800lz@gmail.com>
2014-03-03 16:27:36 +0800

    用一张图片替换了纯色。但是在不同的机器上还有问题。

--- src/org/ginryan/drawarcs/ArcProgressCircle.java
+++ src/org/ginryan/drawarcs/ArcProgressCircle.java
@@ -2,10 +2,14 @@ package org.ginryan.drawarcs;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
+import android.graphics.Region;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -38,19 +42,21 @@ public class ArcProgressCircle extends View {
 		final TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.ArcProgressCircle, defStyle, 0);
 
 		mArcColor = a.getColor(R.styleable.ArcProgressCircle_arcColor, mArcColor);
-		mArcRad = a.getFloat(R.styleable.ArcProgressCircle_arcRad, 90);
+		mArcRad = a.getFloat(R.styleable.ArcProgressCircle_arcRad, 0);
 		if (a.hasValue(R.styleable.ArcProgressCircle_arcDrawable)) {
 			mArcDrawable = a.getDrawable(R.styleable.ArcProgressCircle_arcDrawable);
 			mArcDrawable.setCallback(this);
 		}
 
 		a.recycle();
-
 		mPaint = new Paint();
 		mPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+		mPaint.setAntiAlias(true);
 		mPaint.setTextAlign(Paint.Align.LEFT);
 		rectf = new RectF(0, 0, 0, 0);
 		invalidateTextPaintAndMeasurements();
+		bitmapbg = (BitmapDrawable) getResources().getDrawable(R.drawable.image27);
+		canvasBackg = bitmapbg.getBitmap();
 	}
 
 	private void invalidateTextPaintAndMeasurements() {
@@ -62,10 +68,13 @@ public class ArcProgressCircle extends View {
 		}
 	}
 
+	BitmapDrawable bitmapbg;
+	private Bitmap canvasBackg;
+	Path path = new Path();
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-
 		int paddingLeft = getPaddingLeft();
 		int paddingTop = getPaddingTop();
 		int paddingRight = getPaddingRight();
@@ -73,9 +82,8 @@ public class ArcProgressCircle extends View {
 
 		int contentWidth = getWidth();
 		int contentHeight = getHeight();
-		rectf.set(paddingLeft, paddingTop, contentWidth - paddingRight, contentHeight - paddingBottom);
-		canvas.drawArc(rectf, -90, mArcRad, true, mPaint);
-
+		getSectorClip(canvas, contentWidth / 2, contentHeight / 2, (contentHeight - paddingLeft - paddingRight) / 2, 0, mArcRad);
+		canvas.drawBitmap(canvasBackg, 0, 0, mPaint);
 	}
 
 	public void setPercentageProgress(float percent) {
@@ -87,5 +95,40 @@ public class ArcProgressCircle extends View {
 		mArcRad %= 360;
 		Log.i("arc", "Rad:" + rad);
 		invalidate();
+	}
+
+	/**
+	 * 
+	 * 返回一个扇形的剪裁区
+	 * 
+	 * @param canvas
+	 *            //画笔
+	 * @param center_X
+	 *            //圆心X坐标
+	 * @param center_Y
+	 *            //圆心Y坐标
+	 * @param r
+	 *            //半径
+	 * @param startAngle
+	 *            //起始角度
+	 * @param sweepAngle
+	 *            //终点角度
+	 */
+	private void getSectorClip(Canvas canvas, float center_X, float center_Y, float r, float startAngle, float sweepAngle) {
+		Path path = new Path();
+		// 下面是获得一个三角形的剪裁区
+		path.moveTo(center_X, center_Y); // 圆心
+		path.lineTo((float) (center_X + r * Math.cos(startAngle * Math.PI / 180)), // 起始点角度在圆上对应的横坐标
+				(float) (center_Y + r * Math.sin(startAngle * Math.PI / 180))); // 起始点角度在圆上对应的纵坐标
+		path.lineTo((float) (center_X + r * Math.cos(sweepAngle * Math.PI / 180)), // 终点角度在圆上对应的横坐标
+				(float) (center_Y + r * Math.sin(sweepAngle * Math.PI / 180))); // 终点点角度在圆上对应的纵坐标
+		path.close();
+		// //设置一个正方形，内切圆
+		RectF rectF = new RectF(center_X - r, center_Y - r, center_X + r, center_Y + r);
+		// 下面是获得弧形剪裁区的方法
+
+		path.addArc(rectF, startAngle, sweepAngle - startAngle);
+		canvas.clipPath(path, Region.Op.REPLACE);
+
 	}
 }
